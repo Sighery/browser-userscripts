b8ec8b25ff15bea31e7c711d3b03a9c667b5de54
Sighery <11218602+Sighery@users.noreply.github.com>
2026-05-06 12:30:53 +0200

    Improve Steam age bypass to set cookies directly
    Since it doesn't rely on the DOM any more, we can also just run at
    `document-start` to be as fast as possible. Turned out the cookies were
    quite simple. The main one is `birthtime` which is a normal Unix
    timestamp (no milliseconds). `lastagecheckage` is used to, to remember
    the last chosen date in the UI. I am setting it too just because, but
    overall it won't be needed.

--- SteamAgeCheckBypass.user.js
+++ SteamAgeCheckBypass.user.js
@@ -1,21 +1,45 @@
 // ==UserScript==
 // @name         Steam Age Check Bypass
 // @namespace    Sighery
-// @version      0.1
+// @version      0.2
 // @description  Steam Age Check page bypass
 // @author       Sighery
-// @match        https://store.steampowered.com/agecheck/*
+// @match        https://store.steampowered.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=store.steampowered.com
-// @grant        none
+// @grant        GM_cookie
+// @run-at       document-start
 // ==/UserScript==
 
-(function () {
+(async function () {
   'use strict';
 
-  const year = document.querySelector("select#ageYear");
-  const button = document.querySelector("a#view_product_page_btn");
+  const cookiesSet = await areAgeCookiesSet();
+  if (!cookiesSet) {
+    console.warn("Setting age cookies");
+    await setAgeCookies();
+  }
 
-  year.selectedIndex = 80;
-
-  button.click();
+  if (window.location.href.includes("/agecheck/")) {
+    window.location.href = window.location.href.replace("/agecheck", "");
+  }
 })();
+
+
+async function areAgeCookiesSet() {
+  try {
+    const cookie = await GM_cookie.list({ name: "birthtime", path: "/" });
+    if (cookie !== undefined) {
+      return true;
+    }
+  } catch (e) {
+    return false;
+  }
+
+  return false;
+}
+
+
+async function setAgeCookies() {
+  await GM_cookie.set({ name: "birthtime", value: "315529201", path: "/" });
+  await GM_cookie.set({ name: "lastagecheckage", value: "1-January-1980", path: "/" });
+}
